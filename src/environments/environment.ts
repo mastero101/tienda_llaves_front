8048baccdcbfec05ff6eaa213c440fa8fcd97aea
mastero101 <mastershift_101@hotmail.com>
2025-01-14 04:43:50 -0600

    cambiada url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   mercadoPagoPublicKey: 'TEST-d451af4f-5352-4765-8fe4-2e57fc3b3dd7',
-  apiUrl: 'http://localhost:3000',
+  apiUrl: 'https://tienda-llaves-back.vercel.app',
 }; 
